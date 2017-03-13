fb703515e85906d41fe2fd2bb4d53b1deee24d95
Security2431 <Artem2431@gmail.com>
2017-03-13 15:53:23 +0200

    add use strict

--- app/js/main.js
+++ app/js/main.js
@@ -1,110 +1,53 @@
-// Old browser notification
-$(function() { 
-  $.reject({
-    reject: {
-      msie: 9
-    },
-    imagePath: 'img/icons/jReject/',
-    display: [ 'chrome','firefox','safari','opera' ],
-    closeCookie: true,
-    cookieSettings: {
-      expires: 60*60*24*365
-    },
-    header: 'Ваш браузер устарел!',
-    paragraph1: 'Вы пользуетесь устаревшим браузером, который не поддерживает современные веб-стандарты и представляет угрозу вашей безопасности.',
-    paragraph2: 'Пожалуйста, установите современный браузер:',
-    closeMessage: 'Закрывая это уведомление вы соглашаетесь с тем, что сайт в вашем браузере может отображаться некорректно.',
-    closeLink: 'Закрыть это уведомление',
-  });
-});
+(function($) {
+  "use strict"; // Start of use strict
 
-// jQuery for page scrolling feature - requires jQuery Easing plugin
-/*$(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
+  $(window).load(function(){
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
+      $('body').addClass('ios');
+    };
+    $('body').removeClass('loaded'); 
+  });
+
+  // Old browser notification
+  $(function() { 
+    $.reject({
+      reject: {
+        msie: 9
+      },
+      imagePath: 'img/icons/jReject/',
+      display: [ 'chrome','firefox','safari','opera' ],
+      closeCookie: true,
+      cookieSettings: {
+        expires: 60*60*24*365
+      },
+      header: 'Ваш браузер устарел!',
+      paragraph1: 'Вы пользуетесь устаревшим браузером, который не поддерживает современные веб-стандарты и представляет угрозу вашей безопасности.',
+      paragraph2: 'Пожалуйста, установите современный браузер:',
+      closeMessage: 'Закрывая это уведомление вы соглашаетесь с тем, что сайт в вашем браузере может отображаться некорректно.',
+      closeLink: 'Закрыть это уведомление',
     });
-});*/
-
-
-// Fixed navbar on Scroll
-/*if(!$('.navbar-toggle').is(':visible')) {
-  $('.navbar').affix({
-    offset: {
-      top: $('header').innerHeight()
-    }
-  }); 
-}*/
-
-// Highlight the top nav as scrolling occurs
-/*$('body').scrollspy({
-    target: '.navbar-fixed-top'
-})*/
-
-// Navbar class active
-/*$(document).ready( function () {
-  $(".nav li").click( function () {
-    $(".nav li").removeClass("active");
-    $(this).addClass("active");
   });
-});*/
 
-// Dropdowns on hover on desktop
-/*var navbarToggle = '.navbar-toggle'; // name of navbar toggle, BS3 = '.navbar-toggle', BS4 = '.navbar-toggler'  
-$('.dropdown, .dropup').each(function() {
-  var dropdown = $(this),
-    dropdownToggle = $('[data-toggle="dropdown"]', dropdown),
-    dropdownHoverAll = dropdownToggle.data('dropdown-hover-all') || false;
-  
-  // Mouseover
-  dropdown.hover(function(){
-    var notMobileMenu = $(navbarToggle).size() > 0 && $(navbarToggle).css('display') === 'none' && $(document).width() >= 992 ;
-    if ((dropdownHoverAll === true || (dropdownHoverAll === false && notMobileMenu))) { 
-      dropdownToggle.trigger('click');
-    }
+  /* placeholder*/     
+  $('input, textarea').each(function(){
+    var placeholder = $(this).attr('placeholder');
+    $(this).focus(function() { $(this).attr('placeholder', '');});
+    $(this).focusout(function() {       
+      $(this).attr('placeholder', placeholder);       
+    });
   });
-});*/
 
-
-// Close dropdowns on "esc"
-/*$('.dropdown-menu').bind('keydown',function(event) {
-  // ESC = Keycode 27
-  if (event.keyCode == 27) {
-    $(this).parrent().find('.dropdown-toggle').dropdown('toggle');
-  }
-});*/
-
-// Closes the Responsive Menu on Menu Item Click
-/*$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
-});*/
-
-// Equal height
-/*$('.equial').equialHeight();*/
-
-/*$('.slider').slick({
-  dots: true,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 1,
-  adaptiveHeight: true
-});*/
-
-$(document).ready( function () {
   $('.intro__col').hide();
-  $(document).on("click", ".intro__btn", (function(e) {
-    e.preventDefault();
+  $('.intro__btn').on("click", (function(e) {
+    var $this = $(this);
     $('.intro__col').show();
-    $('.intro__btn').hide();
+    $this.hide();
+    e.preventDefault();
   }));
-});
 
-$(document).ready( function() {
   $('.header__scroll').click(function(){
     $("html, body").animate({ scrollTop: $(".intro").offset().top }, 1000);
     return false;
   });
-});
+
+})(jQuery); // End of use strict
